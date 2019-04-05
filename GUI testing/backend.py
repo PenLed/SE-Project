65be1591ed25e7190c88980536cbb41715bba268
alejandro-mendez-7 <47125690+alejandro-mendez-7@users.noreply.github.com>
2019-04-04 22:48:23 -0500

    Managed to connect to DB

--- GUI testing/backend.py
+++ GUI testing/backend.py
@@ -1,7 +1,29 @@
-import sqlite3 
+# cnx = mysql.connector.connect(user='root', password='Catharsys@1993',
+#                               host ='localhost', database='students')
 
-def login():
-	for i in range(3):
-		while True: 
-			username = input("Username: ")
-			password = input("Password: ")
+import mysql.connector
+
+mydb = mysql.connector.connect(
+			host = "localhost",
+			user = "root",
+			password = "Catharsys@1993",
+			database="students"
+	)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT username FROM student_log")
+
+usernames = mycursor.fetchall() 
+usernames =[i[0] for i in usernames]
+
+mycursor.execute("SELECT password FROM student_log")
+
+passwords = mycursor.fetchall() 
+passwords =[i[0] for i in passwords]
+
+
+
+
+# for row in usernames:
+# 	print(row)
--- GUI testing/frontend.py
+++ GUI testing/frontend.py
@@ -3,6 +3,52 @@ from backend import *
 import tkinter.messagebox
 import datetime 
 from PIL import ImageTk, Image
+import backend
+import os
+
+
+  
+def login_verify():
+	username1 = student_username.get()
+	password1 = student_password.get()
+	
+	if username1 in usernames:
+		if password1 in passwords:
+			login_success()
+		else: 
+			password_not_recognized()
+	else: 
+		user_not_found()
+
+def password_not_recognized():
+	global pass_error
+	pass_error = Toplevel()
+	pass_error.geometry("800x600+0+0")
+	Label(pass_error, text="Password not recognized").pack()
+	Button(pass_error, text="OK", command= delete_password_not_recognised).pack()
+
+def user_not_found():
+	global user_not_found
+	user_not_found = Toplevel()
+	user_not_found.geometry("800x600+0+0")
+	Label(user_not_found, text="Username not found").pack()
+	Button(user_not_found, text="OK", command=delete_user_not_found_screen).pack()
+
+def delete_password_not_recognised():
+	pass_error.destroy()
+
+def delete_user_not_found_screen():
+    user_not_found.destroy()
+
+def login_success(): 
+	global main_menu
+	main_menu = Toplevel()
+	main_menu.geometry("800x600+0+0")
+	Label(main_menu, text="LOGIN SUCCESS").pack()
+
+
+
+
 window = Tk()
 window.title("Graphical User Interface")
 window.geometry('800x600+0+0')
@@ -22,8 +68,8 @@ header = Label(window, text="Learning Management System", font=("arial",30,"bold
 L1 = Label(text="Username", font=("arial",20)).place(x=200, y=200)
 L2 = Label(text="Password", font=("arial",20)).place(x=200, y=250)
 student_username = Entry(window, bd=5)
-student_password = Entry(window, bd=5)
+student_password = Entry(window, bd=5, show="*")
 student_username.place(x=400, y=205)
 student_password.place(x=400, y=255)
-
+Button(text="Login", font=("arial", 15), command=login_verify).place(x=400, y=300)
 window.mainloop()
